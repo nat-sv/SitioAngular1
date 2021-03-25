9ed96ebf09d620cd144cb72ee8e78be3816c65b2
nat-sv <201822031@tese.edu.mx>
2021-03-25 16:41:47 -0600

    Cambios para agregar super heroes  en lista dimnamica

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,7 +5,7 @@ export const HEROES: Hero[] = [
 {id: 12, name: "Batman", edad: 30, poder: "Super detective", identidad : "Bruce Wane", universo: "DC"},
 {id: 13, name: "Hulk", edad: 40, poder: "Superfuerza", identidad : "Bruce Baner", universo: "Marvel"},
 {id: 14, name: "ironman", edad: 25, poder: "Armadura", identidad : "Tony Stark", universo: "Marvel"},
-{id: 15, name: "Thor", edad: 1000, poder: "Dios del trueno", identidad : "Thor", universo: "Marvel"},
+{id: 15, name: "Thor", edad: 1000, poder: "Superfuerza", identidad : "Thor", universo: "Marvel"},
 {id: 16, name: "Spiderman", edad: 20, poder: "Poderes aracnidos", identidad : "Piter Parker", universo: "Marvel"},
 {id: 17, name: "Black Widow", edad: 28, poder: "combate cuerpo a cuerpo", identidad : "Natasha Romanof", universo: "Marvel"},
 {id: 18, name: "Flash", edad: 28, poder: "Superfuer velocidad", identidad : "Barri Allen", universo: "DC"},
