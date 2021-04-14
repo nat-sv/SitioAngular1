18e76a517a5de792ef4deb53e7ad75c2ec5f1412
nat-sv <201822031@tese.edu.mx>
2021-04-13 21:00:27 -0500

    se modificaron varios componentes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../Hero';
 
-import {HEROES} from '../mock-heroes';
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,22 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
   selectedHero?: Hero;
 
-  constructor() { }
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
